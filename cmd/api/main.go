75f315016cddf222579cc8630c3039917d945901
rbalajee <rahrkb4@gmail.com>
2025-06-17 23:37:55 +0530

    exponential backoff

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/rahulbalajee/greenlight/internal/data"
+	"github.com/rahulbalajee/greenlight/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer *mailer.Mailer
 }
 
 func main() {
@@ -48,6 +57,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter max RPS")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter max burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "31d5fad652688d", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "eb0e1da89c4225", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.rbalajee.net>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
@@ -62,10 +77,17 @@ func main() {
 
 	logger.Info("database connection pool established")
 
+	mailer, err := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer,
 	}
 
 	err = app.serve()
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require github.com/julienschmidt/httprouter v1.3.0
 require (
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
+	github.com/wneessen/go-mail v0.6.2 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	golang.org/x/time v0.12.0 // indirect
 )
