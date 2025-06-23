1d4ed2cc1dadc25a6069c130ea24725d7c2cdc86
rbalajee <rahrkb4@gmail.com>
2025-06-23 19:04:31 +0530

    cors in action

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,10 @@ type config struct {
 		password string
 		sender   string
 	}
+
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -64,6 +69,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "31d5fad652688d", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "eb0e1da89c4225", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.rbalajee.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
